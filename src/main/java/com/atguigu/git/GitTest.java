b85a85af3eecb026a8fd83d6d7126f11bd0a0591
Ada <adaxjin@gmail.com>
2021-05-26 20:27:04 -0700

    master commit2

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -6,6 +6,6 @@ public class GitTest {
         System.out.println("hello git!");
         System.out.println("hello git!");
         System.out.println("hello git!");
-        System.out.println("fix!");
+        System.out.println("master fix!");
     }
 }
